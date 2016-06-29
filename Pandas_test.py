8a70e8e6749d29759bca274ee50930585fee8e9c
Brent <cbrent0121@gmail.com>
2016-06-29 18:36:08 -0400

    Format csv out files

--- Pandas_test.py
+++ Pandas_test.py
@@ -5,6 +5,24 @@ import datetime
 import calendar
 import csv
 
-file_name = r'D:\Coding\Python\TestFiles\Data\Test1\Set1\TXT\PP BH-649 B3 PB'
+file_name = r'D:\Coding\Python\TestFiles\Results\B3\6 June 2016\20160624 PP BH-649 B3.txt'
 
+in_text = list(csv.reader(open(file_name, 'rt'), delimiter='\t'))
+out_csv_file = r'D:\Coding\Python\TestFiles\Results\B3\6 June 2016\test.csv'
+print(in_text[0])
 
+# if ('Low QC' or 'HIgh QC') in in_text[1000][3]:
+#     print('found')
+#     print(in_text[1000][3])
+# else:
+#     print('not found')
+
+out_csv = csv.writer(open(out_csv_file, 'wt'))
+
+original_filename = in_text[0][in_text[0].index('Original Filename')]
+sample_name = in_text[0][in_text[0].index('Sample Name')]
+component_name = in_text[0][in_text[0].index('Component Name')]
+concentration = in_text[0][in_text[0].index('Calculated Concentration')]
+
+print(sample_name)
+# out_csv.writerow([test_var])
