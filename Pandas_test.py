cd88d181be0f4c1122ee60fa0d362e7b9508215c
Brent <cbrent0121@gmail.com>
2016-06-29 18:58:49 -0400

    Outwrite csv with columns and LQC/HQC values

--- Pandas_test.py
+++ Pandas_test.py
@@ -11,18 +11,22 @@ in_text = list(csv.reader(open(file_name, 'rt'), delimiter='\t'))
 out_csv_file = r'D:\Coding\Python\TestFiles\Results\B3\6 June 2016\test.csv'
 print(in_text[0])
 
-# if ('Low QC' or 'HIgh QC') in in_text[1000][3]:
-#     print('found')
-#     print(in_text[1000][3])
-# else:
-#     print('not found')
-
-out_csv = csv.writer(open(out_csv_file, 'wt'))
+out_csv = csv.writer(open(out_csv_file, 'wt', newline=''))
 
 original_filename = in_text[0][in_text[0].index('Original Filename')]
 sample_name = in_text[0][in_text[0].index('Sample Name')]
 component_name = in_text[0][in_text[0].index('Component Name')]
 concentration = in_text[0][in_text[0].index('Calculated Concentration')]
 
-print(sample_name)
-# out_csv.writerow([test_var])
+headers = [original_filename, sample_name, component_name, concentration]
+out_csv.writerow(headers)
+
+print(in_text[0].index('Sample Name'))
+out_file_opened = csv.writer(open(out_csv_file, 'a', newline=''))
+for rows in in_text:
+    if 'Low QC' in rows[in_text[0].index('Sample Name')]:
+        out_file_opened.writerow([rows[in_text[0].index('Sample Name')]])
+
+    elif 'HIgh QC' in rows[in_text[0].index('Sample Name')]:
+        out_file_opened.writerow([rows[in_text[0].index('Sample Name')]])
+
