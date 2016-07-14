987b5758e7353c88d466f93ced969af0d002ded9
Brent <cbrent0121@gmail.com>
2016-07-13 21:52:45 -0400

    Finish graphing module

--- graph_package.py
+++ graph_package.py
@@ -5,12 +5,19 @@ import itertools
 from pathlib import Path
 import os
 import sys
-plt.style.use('ggplot')
 import calendar
+from data_main import silent_remove
+from PyPDF2 import PdfFileReader, PdfFileMerger
+import shutil
+import glob
+plt.style.use('ggplot')
 
 
 def flip(items, ncol):
     return itertools.chain(*[items[i::ncol] for i in range(ncol)])
+
+"""Make this take a lab name from PYQT since the dictionary is already there
+    and add the Lab name to top of graph as well"""
 
 
 def make_graph(data_csv):
@@ -33,6 +40,13 @@ def make_graph(data_csv):
         list_of_na.to_csv(na_out_csv)
         print('NA')
         sys.exit(0)
+
+    graph_folder = os.path.join(os.path.dirname(data_csv), 'Graphs')
+    try:
+        shutil.rmtree(graph_folder)
+    except FileNotFoundError:
+        pass
+    os.makedirs(graph_folder)
     """Format date name from first entry to add to graph title"""
     month_digits = df['Date'][0]
     year = '20' + month_digits[-2:]
@@ -47,18 +61,16 @@ def make_graph(data_csv):
     )
     grouped = df.groupby(['Component Name', 'Sample Name'])
 
-    list_of_drugs = sorted(list(set(df['Component Name'])))
+    # list_of_drugs = sorted(list(set(df['Component Name'])))
     # print(grouped.get_group((list_of_drugs[0],)))
     for i in grouped.size().index:
         drug_name = i[0]
         QC = i[1]
-        print(QC)
-    for drugs in list_of_drugs:
-        low_qc = grouped.get_group((drugs, 'Low QC'))
-        x = low_qc['Date']
+        drug_group = grouped.get_group(i)
+        y = drug_group['Calculated Concentration']
+        x = drug_group['Date']
         x_range = len(x)
         x_values = np.arange(1, x_range+1)
-        y = low_qc['Calculated Concentration']
 
         """Begin to calculate positive and negative standard deviations"""
         x_range_full_line = x_range + 2
@@ -90,22 +102,47 @@ def make_graph(data_csv):
         plt.ylim(y_mean - y_sd * 4, y_mean + y_sd * 4)
         plt.xticks(x_values, x, rotation='vertical')
         plt.title('{month_name} {QC} {drug_name}'.format(month_name=month_folder_name,
-                                                         QC='Low QC', drug_name=drugs[:-2]))
-        plt.plot(x_values, y, color='blue', marker='o')
+                                                         QC=QC, drug_name=drug_name[:-2]))
 
         """Begin to plot mean and sd values"""
-        plt.plot(x_values_full_line, y_mean_values, 'gray', label='Mean')
-        plt.plot(x_values_full_line, y_1_pos_sd_values, 'g', label='1 SD')
+        plt.plot(x_values_full_line, y_mean_values, 'gold', label='Mean')
+        plt.plot(x_values_full_line, y_1_pos_sd_values, 'lime', label='1 SD')
         plt.plot(x_values_full_line, y_2_pos_sd_values, color='darkviolet', label='2 SD')
-        plt.plot(x_values_full_line, y_3_pos_sd_values, 'r', label='3 SD')
-        plt.plot(x_values_full_line, y_1_neg_sd_values, 'g')
+        plt.plot(x_values_full_line, y_3_pos_sd_values, 'darkred', label='3 SD')
+        plt.plot(x_values_full_line, y_1_neg_sd_values, 'lime')
         plt.plot(x_values_full_line, y_2_neg_sd_values, color='darkviolet')
-        plt.plot(x_values_full_line, y_3_neg_sd_values, 'r')
+        plt.plot(x_values_full_line, y_3_neg_sd_values, 'darkred')
 
+        if QC == 'Low QC':
+            plt.plot(x_values, y, color='blue', marker='o')
+        else:
+            plt.plot(x_values, y, color='red', marker='o')
         plt.legend(ncol=6, fontsize=9,loc='upper center')
-        plt.show()
-        sys.exit(0)
+        graph_name = '{drug_name} {QC}.pdf'.format(drug_name=drug_name, QC=QC)
+        graph_name = graph_name.replace('/', '-')
+        plt.savefig(os.path.join(graph_folder, graph_name))
+        plt.close()
+    combinePDFs(graph_folder)
+    delete_all_graphs(graph_folder)
 
+
+def combinePDFs(path_to_graphs):
+    path_to_graphs.encode('unicode_escape')
+    try:
+        pdf_files = [f for f in os.listdir(path_to_graphs) if f.endswith('pdf')]
+        merger = PdfFileMerger()
+        for filename in pdf_files:
+            merger.append(PdfFileReader(os.path.join(path_to_graphs, filename), 'rb'))
+        merger.write(os.path.join(path_to_graphs, 'Levey-Jennings Graphs.pdf'))
+    except OSError as exception:
+        raise exception.errno
+
+
+def delete_all_graphs(path_to_graphs):
+    list_of_files = glob.glob(os.path.join(path_to_graphs, '*.pdf'))
+    for files in list_of_files:
+        if files != os.path.join(path_to_graphs, 'Levey-Jennings Graphs.pdf'):
+            os.remove(files)
 
 
 make_graph(r'D:\Coding\Python\TestFiles\Results\ADV\June 2016\data.csv')
--- regex_test.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-folder = r'D:\Coding\Python\TestFiles\Results\B3\June 2016\NA_values.csv'
-
-print(Path(folder).parents[0])
